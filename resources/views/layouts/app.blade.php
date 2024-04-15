236c39e4a3999f8632799008e4f03cb7f3d3feeb
skipzenni <skipzenni@gmail.com>
2024-04-15 17:25:51 +0700

    eps2 - Updating Jetstream Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,73 +20,11 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        <header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
-            <div id="header-left" class="flex items-center">
-                <div class="text-gray-800 font-semibold">
-                    <span class="text-yellow-500 text-xl">&lt;YELO&gt;</span> Code
-                </div>
-                <div class="top-menu ml-10">
-                    <ul class="flex space-x-4">
-                        <li>
-                            <a class="flex space-x-2 items-center hover:text-yellow-900 text-sm text-yellow-500"
-                                href="http://127.0.0.1:8000">
-                                Home
-                            </a>
-                        </li>
-
-                        <li>
-                            <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                                href="http://127.0.0.1:8000/blog">
-                                Blog
-                            </a>
-                        </li>
-
-                        <li>
-                            <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                                href="http://127.0.0.1:8000/blog">
-                                About Us
-                            </a>
-                        </li>
-
-                        <li>
-                            <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                                href="http://127.0.0.1:8000/blog">
-                                Contact Us
-                            </a>
-                        </li>
-
-                        <li>
-                            <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                                href="http://127.0.0.1:8000/blog">
-                                Terms
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-            <div id="header-right" class="flex items-center md:space-x-6">
-                <div class="flex space-x-5">
-                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                        href="http://127.0.0.1:8000/login">
-                        Login
-                    </a>
-                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
-                        href="http://127.0.0.1:8000/register">
-                        Register
-                    </a>
-                </div>
-            </div>
-        </header>
+        @include('layouts.partials.header')
         <main class="container mx-auto px-5 flex flex-grow">
+            {{$slot}}
         </main>
-
-        <footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 ">
-            <a class="text-gray-500 hover:text-yellow-500" href="">About Us</a>
-            <a class="text-gray-500 hover:text-yellow-500" href="">Help</a>
-            <a class="text-gray-500 hover:text-yellow-500" href="">Login</a>
-            <a class="text-gray-500 hover:text-yellow-500" href="">Explore</a>
-        </footer>
+        @include('layouts.partials.footer')
 
         @stack('modals')
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,10 +17,16 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+    <body class="font-sans antialiased">
+        <x-banner />
+
+        @include('layouts.partials.header')
+        <main class="container mx-auto px-5 flex flex-grow">
+            {{$slot}}
+        </main>
+        @include('layouts.partials.footer')
+
+        @stack('modals')
 
         @livewireScripts
     </body>
