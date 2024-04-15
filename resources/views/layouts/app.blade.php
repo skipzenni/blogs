f7a78cd191b39776581d16c40f5b0483e41249ab
skipzenni <skipzenni@gmail.com>
2024-04-16 06:32:11 +0700

    eps4 - Home Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,9 @@
         <x-banner />
 
         @include('layouts.partials.header')
+
+        @yield('hero')
+
         <main class="container mx-auto px-5 flex flex-grow">
             {{$slot}}
         </main>
