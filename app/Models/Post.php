eddd00928a7bd0a2229fa5a876326b952887b7ec
skipzenni <skipzenni@gmail.com>
2024-04-23 06:00:42 +0700

    eps8 - Admin Panel using Filament 3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,16 +6,30 @@ use Carbon\Carbon;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
-    protected $casts = [
-        'published_at' => 'datetime',
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'image',
+        'body',
+        'published_at',
+        'featured'
     ];
+    // protected $casts = [
+    //     'published_at' => 'datetime',
+    // ];
     public function author() {
         return $this->belongsTo(User::class,'user_id');
+    }
+    public function categories() {
+        return $this->belongsToMany(Category::class);
     }
     public function scopePublished($query) {
         $query->where('published_at','<=',Carbon::now());
