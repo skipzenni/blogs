cde80e918dd9eb344a60503c50a16ba4e1ac3ba6
skipzenni <skipzenni@gmail.com>
2024-05-03 02:30:24 +0700

    eps13  - Roles & Permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +13,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
@@ -18,6 +21,28 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
+    const ROLE_ADMIN = "ADMIN";
+    const ROLE_EDITOR = "EDITOR";
+    const ROLE_USER = "USER";
+    const ROLE_DEFAULT = self::ROLE_USER;
+
+    const ROLES = [
+        self::ROLE_ADMIN => 'Admin',
+        self::ROLE_EDITOR => 'Editor',
+        self::ROLE_USER => 'User',
+    ];
+
+    public function canAccessPanel(Panel $panel) : bool {
+        return $this->can('view-admin',User::class);
+        // return str_ends_with($this->email,'@yourdomain.com') && $this->hasVerifiedEmail();
+    }
+
+    public function isAdmin(){
+        return $this->role === self::ROLE_ADMIN;
+    }
+    public function isEditor(){
+        return $this->role === self::ROLE_EDITOR;
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -27,6 +52,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
