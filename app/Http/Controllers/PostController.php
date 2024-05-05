dbacbb919287a9d380c1eb128c45959e60663286
skipzenni <skipzenni@gmail.com>
2024-05-05 20:35:29 +0700

    eps18 - Caching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index(){
+
+        $categories = Cache::remember('categories',Carbon::now()->addDay(), function () {
+            return Category::whereHas('posts', function($query){
+                $query->published();
+            })->take(10)->get();
+        });
         return view('posts.index',[
             // 'posts' => Post::take(5)->get()
-            'categories' => Category::whereHas('posts', function($query){
-                $query->published();
-            })->take(10)->get()
+            'categories' =>$categories,
         ]);
     }
     public function show(Post $post){
